e68e9ba11d6b6befe116a947669ce78ca10fb50d
gloriapss <88331740+gloriapss@users.noreply.github.com>
2022-02-08 19:01:46 -0600

    Update Game.js

--- js/Game.js
+++ js/Game.js
@@ -37,8 +37,8 @@ class Game {
 
   play(){
     form.hide();
-    textSize(30);
-    text("Game Start", 120, 100)
+    //textSize(30);
+    //text("Game Start", 120, 100)
     Player.getPlayerInfo();
 
     if(allPlayers !== undefined){
@@ -48,31 +48,26 @@ class Game {
       var y;
       //var display_position = 130;
       for(var plr in allPlayers){
-        index = index +1;
-        x = x+200;
-        y = displayHeight-allPlayers[plr].distance;
+        index = index + 1;
+        x = x + 200;
+        y = displayHeight - allPlayers[plr].distance;
         cars[index-1].x = x;
         cars[index-1].y = y;
+        
         if (index === player.index){
           cars[index-1].shapeColor="red";
           camera.position.x = displayWidth/2;
           camera.position.y = cars[index-1].y;
         }
 
-        if(keyIsDown(UP_ARROW)&&player.index!==null){
+        if(keyIsDown(UP_ARROW) && player.index !== null){
           player.distance+=10;
           player.update();
         }
+        
         drawSprites();
         //display_position+=20;
         //textSize(15);
         //text(allPlayers[plr].name + ": " + allPlayers[plr].distance, 120,display_position)
       }
-    }
-
-    if(keyIsDown(UP_ARROW) && player.index !== null){
-      player.distance +=50
-      player.update();
-    }
   }
-}
