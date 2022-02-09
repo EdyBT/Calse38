cb39f23a9c849b4cc1d97b9811d31ce564047199
gloriapss <88331740+gloriapss@users.noreply.github.com>
2022-02-08 18:57:30 -0600

    Update Form.js

--- js/Form.js
+++ js/Form.js
@@ -4,17 +4,19 @@ class Form {
     this.input = createInput("Name");
     this.button = createButton('Play');
     this.greeting = createElement('h2');
+    this.title = createElement('h2');
   }
+  
   hide(){
     this.greeting.hide();
     this.button.hide();
     this.input.hide();
+    this.title.hide();
   }
 
   display(){
-    var title = createElement('h2')
-    title.html("Car Racing Game");
-    title.position(130, 0);
+    this.title.html("Car Racing Game");
+    title.position(displayWidth/2 -50, 0);
 
     this.input.position(displayWidth/2-40, displayHeight/2-80);
     this.button.position(displayWidth/2+30, displayHeight/2);
